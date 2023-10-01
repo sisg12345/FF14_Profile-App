e340c4749949bb139fb2f1aab8fda3d5cc9eb574
ss <sisg12345@yahoo.co.jp>
2023-10-01 17:17:30 +0900

    fix: 画像をビルド対象に使えるための修正
    1. 画像をビルド対象に加えるためにインポートするように修正

--- vue-app/src/components/TopView/MainVisual.vue
+++ vue-app/src/components/TopView/MainVisual.vue
@@ -2,15 +2,29 @@
   <!-- カルーセル -->
   <Carousel
     :id="'main_visual_carousel'"
-    :images="[
-      '/src/assets/images/topView/FF14_tori-cafe-1_1920x600.jpg',
-      '/src/assets/images/topView/FF14_tori-cafe-2_1920x600.jpg',
-      '/src/assets/images/topView/FF14_tori-cafe-3_1920x600.jpg',
-    ]"
+    :images="images"
   />
 </template>
 <script>
+import { reactive } from 'vue'
+// NOTICE: ビルド対象に加えるためにインポートする
+import img1 from "../../assets/images/topView/FF14_tori-cafe-1_1920x600.jpg"
+import img2 from "../../assets/images/topView/FF14_tori-cafe-2_1920x600.jpg"
+import img3 from "../../assets/images/topView/FF14_tori-cafe-3_1920x600.jpg"
+
 export default {
   name: 'MainVisual',
+  setup() {
+    // 画像一覧
+    const images = reactive([
+      img1,
+      img2,
+      img3,
+    ])
+
+    return {
+      images,
+    }
+  },
 }
 </script>
